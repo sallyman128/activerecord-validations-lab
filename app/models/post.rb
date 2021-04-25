0f09db4cccab10de4e0df9e31867ea20a7fb7ea1
Salmaan <salmaan.arif.ali@gmail.com>
2021-04-25 16:39:47 -0400

    commit tree 2cbf54c3c2be0b093e30ed8527d11d9eebecdc6d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :non_clickbait
+
+  def non_clickbait
+    if title.present?
+      errors.add(:title, "not clickbaity") unless title.include?("You Won't")
+    end
+  end
 end
